480bf816d26dec218b18bb61010e77a06b7f8a5f
Olusegun <segun.ekoh@gmail.com>
2020-01-10 12:31:19 +0100

    Number literal, Functions & Constructor syntax

--- topics/Data_Types/Numbers/number.js
+++ topics/Data_Types/Numbers/number.js
@@ -1,1 +1,16 @@
+/* 
+  - Number Literal, Functions & Constructor Syntax
+*/
+// number literal - most recommended
+const literalNumber = 99;
+console.log(literalNumber);
 
+// functions
+console.log(Number(66)); // 66
+// converts a stringed-number to integer
+console.log(Number('66')); // 66
+console.log(Number('44px')); // NaN
+
+/* eslint-disable no-new-wrappers */
+console.log(new Number(43)); // 43
+console.log(new Number('43px')); // NaN
