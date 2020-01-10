da5262463a2adf86612c453d50a556cc533715b9
Olusegun <segun.ekoh@gmail.com>
2020-01-10 19:04:17 +0100

    Objects are fundamental :cool:

--- topics/Data_Types/Objects/object.js
+++ topics/Data_Types/Objects/object.js
@@ -1,1 +1,10 @@
-console.log('It works!');
+/*
+ - Objects are used for collection of data & functionality
+*/
+const person = {
+  first: 'Olusegun',
+  last: 'Ekoh',
+  // eslint-disable-next-line prettier/prettier
+  age: 24
+};
+console.log(person, person.last);
