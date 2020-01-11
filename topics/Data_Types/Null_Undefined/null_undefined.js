470d4f339996f04d3352bc33e16fc04476679b1a
Olusegun <segun.ekoh@gmail.com>
2020-01-11 15:36:31 +0100

    undefined or null?:relaxed:

---
+++ topics/Data_Types/Null_Undefined/null_undefined.js
@@ -0,0 +1,13 @@
+/*
+  Null & Undefined
+
+*/
+// undefined means a variable has been declared but not assigned yet
+// It has a typeof 'undefined'
+let age;
+console.log(age); // undefined
+
+// null is an assigned value; it means nothing,
+// It is an object
+const nothing = null;
+console.log(nothing); // null
