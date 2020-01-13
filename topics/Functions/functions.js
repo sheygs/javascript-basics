1572afe96202c39f439979ad9f9078f9831cd879
Olusegun <segun.ekoh@gmail.com>
2020-01-13 13:15:01 +0100

    arrow functions, IIFE's, methods & callback functions

--- topics/Functions/functions.js
+++ topics/Functions/functions.js
@@ -43,6 +43,7 @@ function doctorize(name) {
 */
 
 /* eslint-disable */
+
 //console.log(doctorize2('Ade')); // cannot access doctorize before initialization
 
 let doctorize2 = function(name) {
@@ -52,5 +53,113 @@ console.log(doctorize2('Kola')); // Dr. Kola
 
 
 /*
-
+3) Arrow Functions
+  - It is shorter syntax of writing function expressions
+  - Implicit return: returns a value without using the "return" keyword
+  - If there's only one argument, remove the parenthesis (optional)
+  - They do not create their own "this" value
+  Steps to create an arrow function:
+    (i) Create a function declaration
+    (ii) convert (i) to a function expression
+    (iii) convert (ii) to an arrow function
 */
+// (i)
+// function inchesToCM(inches) {
+//   return inches * 2.54;
+// }
+
+// (ii)
+// const inchesToCM = function(inches) {
+//   return inches * 2.54;
+// }
+
+// (iii)
+  // const inchesToCM = (inches) => {
+  //   return inches * 2.54;
+  // }
+
+ // (iv)
+// const inchesToCM = (inches) => inches * 2.54;
+
+//(v)
+//  const inchesToCM = inches => inches * 2.54;
+//  console.log(inchesToCM(3));
+
+// returning an object
+// function makeABaby(first, last){
+//   return {
+//     name: `${first.concat(' ',last)}`,
+//     age: 0
+//   }
+// }
+// makeABaby('Ewa','Anjorin');
+
+// //Improved arrow functions
+ const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
+
+
+/* eslint-enable */
+/*
+  4) IIFE - Immediately Invoked Function Expression
+  - It is a way to execute functions immediately, as soon as they are created
+  - can be defined with regular/anonymous/arrow functions
+  - Don't pollute the global object and they isolate variable declarations
+*/
+(function(name) {
+  console.log(`Immediately Invoked by ${name}!`);
+})('Sheygs');
+
+/*
+  5) METHODS
+  - Are functions that live inside an object
+*/
+
+const sheygs = {
+  name: 'Olusegun',
+  /* eslint-disable */
+  // method 1
+  sayHi: function(){ 
+    console.log(this, 'Hey Sheygs!'); // this refers to the object
+  },
+  // short-hand method 2
+  bio(){
+    console.log(this,'I am Olusegun'); // this refers to the object
+  },
+  // Arrow functions
+  whisper: () => {
+    console.log(this, 'Whispering...'); // refers to the Window object
+  }
+};
+console.log(sheygs);
+
+/*
+6) CALLBACK FUNCTIONS
+  - It is a function that is passed as an argument to another function
+  to be called at a later time
+  - A function that accepts a function as an argument
+  is a "higher-order function"
+*/
+
+// Click callbacks
+const button = document.querySelector('.click-me');
+// method 1
+// button.addEventListener('click', sheygs.whisper);
+
+// method 2
+// function handleClick(){
+//   console.log(this);
+// }
+// button.addEventListener('click', handleClick);
+
+// method 3
+// "this" refers to the window object
+// button.addEventListener('click', () => console.log(this)); 
+
+// "this" refers to the button
+button.addEventListener('click', function () { 
+  console.log(this, 'Nice!');
+});
+
+
+// Timer callbacks
+setTimeout(function(){ console.log(this) }, 3000); // "this" refers to the Window object
