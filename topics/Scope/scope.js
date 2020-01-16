135b88f9bbea2be83760db5e18608d44e78cb8dc
Olusegun <segun.ekoh@gmail.com>
2020-01-16 11:51:48 +0100

    Add Debug file/folder

---
+++ topics/Scope/scope.js
@@ -0,0 +1,5 @@
+/*
+  - Scope of a variable roughly means "the availability of a variable in a code"
+  - Global variables (with the exception of "let" & "const") are attached to the window object
+  - Scoping types are: global, function/local, and blocked
+*/
