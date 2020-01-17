f0530905f426ae0e1fb13d252c76be48195d9623
Olusegun <segun.ekoh@gmail.com>
2020-01-17 10:35:00 +0100

    update notes on variable declarations :pencil:

--- topics/Variables_Statements/index.js
+++ topics/Variables_Statements/index.js
@@ -7,37 +7,49 @@
  3 ways to declare a variable are: var, let & const
  -------------------------------------------------------------------
  -> var
-    - Can be initialized
-    - Can be updated
-    - function scoped; only available inside the parent function
+    - Can be declared
     - Can be redeclared inside of its  scope i.e. can be declared inside of its scope more than once
+    - Can be updated/reassigned
+    - function scoped; only available inside the parent function
+
  -> let
-    - Can be initialized 
-    - Can be updated
-    -  blocked scoped {}
+    - Can be declared
     -  Can't be redeclared inside of its scope i.e. can only be declared
     inside of its scope once
+    - Can be updated
+    -  blocked scoped {}
+   
  -> const
-    - Can't be initialized (MUST be assigned a value i.e. declared)
-    - Can't be updated
-    - blocked scoped {}
+    - Can't be declared (MUST be initialised/assigned a value)
     - Can't be redeclared inside of its scope i.e. can only be declared
     inside of its scope once.
+    - Can't be updated
+    - blocked scoped {}
     -----------------------------------------------------------------
+
     - Variable declaration statements in JS are similar 
     to statements in English
     - Variable declaration statements are used to "do" something
     - Variable declaration statements end in a semi-colon (;) 
 
+    - The following is a sequence in which variable declaration & initialisation occurs.
+      
+      Declaration -> Initialisation/Assignment -> Usage
+     
     -----------------------------------------------------------------
+
     -The purpose of  'use strict' is to indicate that the code
     -should be executed in "strict mode"
     -With strict mode, you cannot, for example use undeclared variables
+
     -----------------------------------------------------------------
+
     Which keywords should I use ?
     1) Use 'const' by default
     2) Use 'let' if the value of the variable is to be changed
+
     ------------------------------------------------------------------
+
     NAMING VARIABLES
     - Names should begin with a lowercase string
     - Names cannot contain symbols or begin with a symbol except $ and _
@@ -50,18 +62,21 @@
 // line below necessary to disable eslint rules  for illustration
 /* eslint-disable */ 
 'use strict';
-var name;  //variable initialization
-console.log(name);  //returns undefined
+var name;  // declaration
+console.log(name);  // undefined
 
-var first = 'Olusegun'; //variable declaration statement
-first = 'Segun'; //variable reassignment
+var first = 'Olusegun'; // Declaration & Assignment
+first = 'Segun'; // Reassignment
 
-let age = '24'; //variable declaration
-age = '25'; //variable reassignment
+let age = '24'; // Declaration & Initialisation
+age = '25'; // Reassignment
+
+let a; // Declaration
+a = 100; // Assignment
+console.log(a); // usage
 
 const gender = 'Male';
 //gender = 'Female'; //error, can't reassign const variables
-
 
 //isMarried = false;  
 //console.log(isMarried); //returns undefined in strict mode, otherwise, false
