e19e8d8943470e82e45ff6d0d4c9f03a5586c757
Olusegun <segun.ekoh@gmail.com>
2020-02-03 14:11:30 +0100

    BEDMAS

---
+++ topics/Conditional_Logic/conditional-1.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/*
+B - Bracket ()
+E - Exponential **
+D - Division /
+M - Multiplication *
+A - Addition +
+S - Subtraction -
+*/
+console.log( 45 * (2 ** 4) / 5 + 5 - 3); // 146
