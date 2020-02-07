60b27c8494d0df3559911c8b1d5fd2996dac1c32
Olusegun <segun.ekoh@gmail.com>
2020-02-07 15:07:09 +0100

    switch-case implementation of animating a turtle using CSS variables

---
+++ topics/Conditional_Logic/switch-5.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+const turtle = document.querySelector('.turt');
+let x = 0,
+    y = 0,
+    flipped = false,
+    rotate = 0;
+const speed = 10;
+function handleKeyDown(event){
+  // if it's not an arrow key, ignore
+ if (!event.key.includes('Arrow')) { return; }
+  switch(event.key){
+      case 'ArrowUp':
+           y--;
+           rotate = -90;
+           break;
+      case 'ArrowDown':
+           y++;
+           rotate = 90;
+           break;
+      case 'ArrowLeft':
+           x--;
+           rotate = 0;
+           flipped = true;
+           break;
+      case 'ArrowRight':
+           x++;
+           rotate = 0;
+           flipped = false;
+           break;
+      default:
+           alert('Invalid Move!');
+           break;
+  }
+   turtle.setAttribute('style',`
+   --rotateX: ${flipped ? '180deg' : '0'};
+   --rotate: ${rotate}deg;
+   --x: ${x * speed}px;
+   --y: ${y * speed}px
+   `);
+
+   console.log(x,y);
+}
+
+window.addEventListener('keydown', handleKeyDown);
