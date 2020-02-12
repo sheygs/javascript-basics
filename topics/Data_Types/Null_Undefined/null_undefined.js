34986713b44ffb1c9fa676eb365f5f408828662a
Olusegun <segun.ekoh@gmail.com>
2020-02-12 16:55:37 +0100

    lean file

--- topics/Data_Types/Null_Undefined/null_undefined.js
+++ topics/Data_Types/Null_Undefined/null_undefined.js
@@ -1,14 +1,9 @@
 /*
   Null & Undefined
-
 */
-// undefined means a variable has been declared but not assigned yet
-// It has a typeof 'undefined'
 let age;
 console.log(age); // undefined
 
-// null is an assigned value; it means nothing,
-// It is an object
 const nothing = null;
 console.log(nothing); // null
 
@@ -16,7 +11,7 @@ console.log(nothing); // null
 const walter = {
   first: 'Walter'
 };
-console.log(walter.last);
+console.log(walter.last); // undefined
 
 const teller = {
   first: 'Raymond',
