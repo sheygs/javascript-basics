0b4870b3102d3b68aa6a14c52efefc59cbe59b3c
Olusegun <segun.ekoh@gmail.com>
2020-02-13 09:41:29 +0100

    :fire:

--- topics/Scope/scope.js
+++ topics/Scope/scope.js
@@ -1,41 +1,30 @@
-/*
-  - Scope of a variable roughly means "the availability of variables in a code"
-  - functions can also be scoped to their parent functions
-  - Global variables (with the exception of "let" & "const") are attached to the window object
-  - Scoping types are: global, function/local, and blocked
-*/
-
 /* eslint-disable */
 
-/*
- N/B:
-- Anything in the global scope is attached to the Window object 
-- with the exception of 'let' & 'const'
-*/
 var first = 'Olusegun';  // window.first => 'Olusegun';
 let age = 24; // window.age => undefined
 const last = 'Olaoluwa'; // window.last => undefined
 
-// Function Scope
+/* Function Scope */
 
-// function go(){
-//   const hair = 'blonde';
-// // scope look-up for age in the global scope as age is not found in local go()
-//   console.log(hair, age); // blonde, 24
-// }  
-// go();
-// console.log(age); // 24
+function display() {
+  const hair = 'blonde';
 
-
-// function go(){
-//   const hair = 'blonde';
-//   const myAge = 25;
-//   console.log(hair, myAge);
-//   console.log(age);
-// }
+  // scope look-up for age  
+  console.log(hair, age); // blonde, 24
+}  
+display();
+console.log(age); // 24
 
 
-function isCool (name){
+function go() {
+  const hair = 'blonde';
+  const myAge = 25;
+  console.log(hair, myAge);
+  console.log(age);
+}
+
+
+function isCool(name) {
   // let cool;  
   if (name === 'Sheygs'){
      var cool = true;  // or cool = true;
@@ -46,33 +35,33 @@ function isCool (name){
  isCool('Sheygs');
 
 
- function logOutput(){
+ /* blocked-scoping with var */
+ function logOutput() {
   for (var i = 0; i < 9; i++){
     console.log(i);
   }
-  console.log(i); // i = 9; i is leaked out of the blocked-scope
+  // i is leaked out 
+  console.log(i); // i = 9
  }
 
  logOutput();
 
 
- // Scope look-up
+ /* Scope look-up */
  const dog = 'Tim';
 
-// lexical scope/ static scope: simply means scope look-up happens
-// where the function is defined and not where they are called/invoked
  function logDog(dog){
    console.log(dog);
  }
 
- function go(){
+ function log(){
    const dog = 'sunny';
    logDog(dog);
  }
 
-go();
+log();
 
-// function scoping
+/* function scoping */
 function sayHi(name){
   function yell(){
     console.log(`${name.toUpperCase()} yelling...`);
@@ -81,5 +70,5 @@ function sayHi(name){
 }
 
 sayHi('Kkols');
-// yell();  Reference Error: yell is not defined
-// N/B: function are going to be scoped to the parent function
+yell();  // reference error; yell() out of reach
+
