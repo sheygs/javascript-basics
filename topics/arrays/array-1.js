bb676eb8e1fb8ba423d3e688b9e906f740b196f3
Olusegun <segun.ekoh@gmail.com>
2020-02-17 23:55:24 +0100

    Arrays :zap:

---
+++ topics/arrays/array-1.js
@@ -0,0 +1,18 @@
+// Are zero-based index i.e. starting at zero
+// used for holding an item where position matters
+// creating an array  - [] or new Array()
+//getting the length of an array
+// accessing arrays - using their indexes
+//mutable/immutable methods
+
+// creating an array
+let list = new Array(5);
+const names = ['Olusegun', 'John', 'Adebisi'];
+
+//getting the length of an array
+const len = names.length;
+
+// accessing items in an arrays
+const firstName = names[0];
+const lastName = names[len - 1];
+
