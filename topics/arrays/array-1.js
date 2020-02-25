d2a33e377e295dfa92e4f7d6b5127054b0cac7f5
Olusegun <segun.ekoh@gmail.com>
2020-02-25 01:32:09 +0100

    arrays: mutable vs immutable

--- topics/arrays/array-1.js
+++ topics/arrays/array-1.js
@@ -1,13 +1,17 @@
-// Are zero-based index i.e. starting at zero
-// used for holding an item where position matters
-// creating an array  - [] or new Array()
-//getting the length of an array
-// accessing arrays - using their indexes
-//mutable/immutable methods
+const names = ['Olusegun', 'John', 'Adebisi'];
+const countries = ['Nigeria', 'United Kingdom', 'Canada', 'France'];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const bikes = ['bianchi', 'miele', 'panasonic', 'miyata'];
+const comments = [
+  {text: 'Cool beans', id: 100},
+  {text: 'Not Nice', id: 200},
+  {text: 'Really Awesome', id: 300},
+  {text: 'Bad Idea', id: 400},
+  {text: 'Cool Work', id: 500}
+]
 
 // creating an array
 let list = new Array(5);
-const names = ['Olusegun', 'John', 'Adebisi'];
 
 //getting the length of an array
 const len = names.length;
@@ -16,3 +20,79 @@ const len = names.length;
 const firstName = names[0];
 const lastName = names[len - 1];
 
+/* Mutable Methods */
+
+// create a copy of the original array
+// before applying a mutable method
+// so that it doesn't modify the original data
+
+// Method 1. Using spread syntax
+const numbersReversed = [...numbers].reverse();
+
+// Method 2. Using slice() method
+const numbersSliced = numbers.slice(0).reverse();
+
+console.log(numbers);
+console.log(numbersReversed);
+console.log(numbersSliced);
+
+// const addName = names.push('Katrine');
+const add = [...names,'Katrine'];
+
+console.log(names);
+console.log(add);
+
+// const addToFront = countries.unshift('Iraq');
+const immutableAddToFront = ['Brazil', ...countries];
+
+console.log(countries);
+console.log(immutableAddToFront);
+
+
+/* Immutable Method */
+
+const part = numbers.slice(3);
+console.log(numbers);
+console.log(part);
+
+// add an item to the middle of an array
+
+const newBikes = [
+  ...bikes.slice(0, 2),
+  'Sonyata',
+  ...bikes.slice(-2)
+]
+console.log(bikes);
+console.log(newBikes);
+
+// remove an item (4th-item)
+const modifiedBikes = [
+  ...newBikes.slice(0, 3),
+  ...newBikes.slice(4)
+]
+console.log(newBikes);
+console.log(modifiedBikes);
+
+
+// delete a comment from a given id
+
+// Method 1
+// function deleteComment(id, comments){
+//   const index = comments.findIndex(comment => comment.id === id);
+//   return [
+//     ...comments.slice(0, index),
+//     ...comments.slice(index + 1)
+//   ]
+// }
+
+// Method 2
+function deleteComment(id, comments){
+  const index = comments.findIndex(comment => comment.id === id);
+  const commentsCopy = [...comments];
+  commentsCopy.splice(index, 1);
+  return commentsCopy;
+}
+
+const result = deleteComment(500, comments);
+console.log(comments);
+console.log(result);
--- topics/arrays/array.md
+++ topics/arrays/array.md
@@ -2,10 +2,11 @@
 - Arrays are zero-based index.
 - Arrays are used for holding large set of datas where position matters.
 - __Mutable__ Methods changes the original data/array.
-  - `push`, `pop`, `shift`, `unshift`, `splice`, `reverse`
+  - `push`, `pop`, `shift`, `unshift`, `splice`, `reverse`, `sort`
 
 - __Immutable__ Methods do not change the original data/array.
-  - `slice`, `join`
+  - `slice`, `join`, `concat`
+
 - Take a _copy_ of the original data if you use mutable methods without wanting to modify the original data/array.
 - To create an array, use the bracket symbol `[]` of the `new Array ()` method.
 
