2562cd273001ce3933d7d0b3ffb4fbada31c8a9d
sheygs13 <segun.ekoh@gmail.com>
2020-04-03 18:57:22 +0100

    finish shopping list app :rocket:

--- exercises/shopping-list/shopping.js
+++ exercises/shopping-list/shopping.js
@@ -1,7 +1,7 @@
 // define UI variables
 const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
-const items = []; // state of our app
+let items = []; // state of our app
 
 const event = new CustomEvent('updateItems');
 
@@ -28,9 +28,16 @@ function displayItems(){
   const mappedItems = items.map(item => {
     return `
       <li class="shopping-item">
-        <input type="checkbox"> 
+        <input type="checkbox"
+         value="${item.id}"
+         ${item.isComplete && 'checked'}
+        > 
         <span> ${item.name}</span>
-        <button aria-label="Remove ${item.name}">&times;</button>
+        <button 
+        aria-label="Remove ${item.name}"
+        value="${item.id}"
+        >
+        &times;</button>
       </li>
     `
   }).join('');
@@ -42,6 +49,46 @@ function addToLocalStorage(){
   localStorage.setItem('items', JSON.stringify(items));
 }
 
+function populateItemsFromLocalStorageOnPageLoad(){
+  const listItems = JSON.parse(localStorage.getItem('items'));
+  if (listItems.length){
+    items.push(...listItems);
+    list.dispatchEvent(event);
+  }
+}
+
+
+function deleteItem(id){
+  console.log('Deleting Item', id);
+  items = items.filter(item => item.id !== id);
+  list.dispatchEvent(event);
+}
+
+
+function markAsComplete(id){
+  console.log('Marking as complete', id);
+  const itemRef = items.find(item => item.id === id);
+  itemRef.isComplete = !itemRef.isComplete;
+  list.dispatchEvent(event);
+  console.log(itemRef);
+}
+
 shoppingForm.addEventListener('submit', e => handleSubmit(e));
 list.addEventListener('updateItems', displayItems);
 list.addEventListener('updateItems', addToLocalStorage);
+
+
+// event delegation
+list.addEventListener('click', function(e){
+   const id = Number(e.target.value);
+   if (e.target.matches('button')){
+     deleteItem(id);
+   }
+
+   if (e.target.matches('input[type="checkbox"]')){
+     markAsComplete(id);
+   }
+});
+
+// run on page load
+populateItemsFromLocalStorageOnPageLoad();
