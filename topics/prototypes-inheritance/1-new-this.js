60e05e306fb202606631f4d2e335cf5306461ccd
sheygs13 <segun.ekoh@gmail.com>
2020-04-10 15:21:01 +0100

    this keyword usages

--- topics/prototypes-inheritance/1-new-this.js
+++ topics/prototypes-inheritance/1-new-this.js
@@ -1,19 +1,81 @@
 // The 'new' keyword is used to create an instanceof an object
+
+// dates don't  have literal syntax
 const date = new Date();
-console.log(date.getFullYear());
+//console.log(date.getFullYear());
 
 const names = new Array('Olusegun', 'Ekoh');
 const sheygs = new Object({ first: 'Olusegun', last:'Ekoh' });
 const span = document.createElement('span');
-console.dir(names);
-console.dir(sheygs);
-console.dir(span);
 
-function Pizza(){
- console.log('Pizza');
+//console.dir(names);
+//console.log(sheygs)
+//console.log(span);
+
+
+function Pizza(toppings = [], customer){
+ console.log('Make a pizza');
+ this.toppings = toppings;
+ this.customer = customer;
+ this.id = Math.floor(Math.random()*16777215).toString(16);
 }
 
-const pepperoni = new Pizza();
-console.log(pepperoni);
-console.log(pepperoni.constructor);
-console.log(pepperoni instanceof Pizza);
+const pepperoniPizza = new Pizza(['hamburger', 'pepperoni'], 'Olusegun');
+const anotherPizza = new Pizza(['mushrooms'], 'Ewa');
+
+console.log(pepperoniPizza);
+console.log(anotherPizza);
+// console.log(pepperoni.constructor);
+// console.log(pepperoni instanceof Pizza);
+
+
+// 'this' keyword refers to the instanceof an object that a function is bound
+// The 'this' keyword in an arrow function is equal to whatever the 'this' 
+// keyword at a higher level function, else the 'this' keyword is equal to
+// the 'window' object.
+
+const button1 = document.querySelector('.one');
+const button2 = document.querySelector('.two');
+
+
+/* using the old way */
+
+// function handleClick (){
+//  // 'this' refers to the buttons
+//  console.log('Outside:',this);
+
+//  let that = this;
+ 
+//  setTimeout(function(){
+
+//   // 'this' refers to the window object
+//   console.log('Inside:',this);
+
+//   // to make 'this' refer to the button in this scope, 
+//   // set 'that' to equal to the upper-scoped 'this' 
+//   // which refers to the actual buttons
+//    that.textContent = 'Content Changed';
+
+//  }, 2000);
+// }
+
+/* the new approach using arrow-function */
+
+function handleClick(){
+
+  // this refers to the buttons
+  console.log('Outside:',this);
+
+  setTimeout(() => {
+
+    // 'this' refers to the buttons too!
+    // This is because the 'this' keyword in 
+    // an arrow function is equal to whatever the 'this' 
+    // keyword at a higher level function
+    console.log('Inside:',this);
+    this.textContent = 'Button Clicked';
+  }, 2000);
+}
+
+button1.addEventListener('click', handleClick);
+button2.addEventListener('click', handleClick);
