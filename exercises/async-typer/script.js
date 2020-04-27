f2cd9616e806c3045c9e1a38885c1fb333ca5cd7
sheygs13 <segun.ekoh@gmail.com>
2020-04-27 22:39:10 +0100

    async-typer recursive method

--- exercises/async-typer/script.js
+++ exercises/async-typer/script.js
@@ -7,17 +7,39 @@ function getRandomBtw(min = 20, max = 100, random = Math.random()){
  return Math.floor(random * (max - min + 1)) + min;
 }
 
-async function draw(element){
- const text = element.textContent;
- const { typeMin, typeMax } = element.dataset;
- let string = '';
- for (const char of text){
-    console.log(string);
-    string+=char;
-    element.textContent = string;
-    await wait(getRandomBtw(typeMin, typeMax));
+
+// iterative approach
+
+// async function draw(el){
+//  const text = el.textContent;
+//  const { typeMin, typeMax } = el.dataset;
+//  let string = '';
+//  for (const char of text){
+//     console.log(string);
+//     string+=char;
+//     el.textContent = string;
+//     await wait(getRandomBtw(typeMin, typeMax));
+//  }
+// }
+
+
+
+// recursive approach
+
+function draw(el){
+ let index = 1;
+ const text = el.innerText;
+ const { typeMin, typeMax } = el.dataset;
+ async function drawLetter(){
+    const length = text.length - 1;
+    el.textContent = text.slice(0, index);
+    index++;
+    await wait(getRandomBtw(typeMin,typeMax));
+    if (index <= length) {
+     drawLetter();
+    }
  }
-  
+  drawLetter();
 }
 
 document.querySelectorAll('[data-type]').forEach(draw);
