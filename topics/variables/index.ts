bd77b43e132a6385221d6c3c7d7085f309899a24
sheygs13 <segun.ekoh@gmail.com>
2020-05-13 09:20:59 +0100

    refactor to typescript

---
+++ topics/variables/index.ts
@@ -0,0 +1,27 @@
+// declaration
+var fullName: any;
+// undefined
+console.log(fullName);
+
+// declaration & assignment
+var first: string = 'Olusegun';
+// re-assignment
+first = 'Segun';
+
+// declaration & initialisation
+let age: string = '24';
+// reassignment
+age = '25';
+
+// declaration
+let a: number;
+// assignment
+a = 100;
+// usage
+console.log(a);
+
+const gender: string = 'Male';
+// gender = 'Female';
+
+const isMarried: boolean = false;
+console.log(isMarried); 
